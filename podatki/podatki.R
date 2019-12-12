faedda6a9bc1599fad9d5acb5563331e122abba0
Matevz Kopac <kopac.matevz98@gmail.com>
2019-12-12 10:05:18 +0100

    vaje

--- podatki/podatki.R
+++ podatki/podatki.R
@@ -1,7 +1,7 @@
 library(readr)
-moji_podatki <- read_delim("podatki/goli_podatki.csv", 
+moji_podatki1 <- read_delim("podatki/goli_podatki.csv", 
                            ";", escape_double = FALSE, trim_ws = TRUE, locale=locale(encoding="windows-1250"), 
                            na=c("#VALUE!","/"))
-View(moji_podatki)
+View(moji_podatki) 
 
                          
