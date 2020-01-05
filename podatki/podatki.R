02607e3240ae33dd618230b80873de7701d45b15
Matevž Kopač <kopac.matevz98@gmail.com>
2020-01-05 17:43:41 +0100

    uvoz podatkov, tabeli

--- podatki/podatki.R
+++ podatki/podatki.R
@@ -1,7 +1,9 @@
 library(readr)
-moji_podatki1 <- read_delim("podatki/goli_podatki.csv", 
+moji_podatki <- read_delim("podatki/goli_podatki.csv", 
                            ";", escape_double = FALSE, trim_ws = TRUE, locale=locale(encoding="windows-1250"), 
-                           na=c("#VALUE!","/"))
-View(moji_podatki) 
+                           na=c("#VALUE!","/", "0",""))
+osnovni_podatki <- moji_podatki[c(1:632),c(4,8,10,11,13,14)]
 
-                         
+stevilski_podatki <- moji_podatki[c(1:632),c(4,22:29,32,34,36,40,41)]
+
+
